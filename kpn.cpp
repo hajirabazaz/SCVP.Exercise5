384674ac812beddae4d1760aae258b955b07e0c8
heg66did <heg66did@rhrk.uni-kl.de>
2023-01-28 15:49:35 +0100

    task complete

--- kpn.cpp
+++ kpn.cpp
@@ -4,3 +4,39 @@
 #include "kpn.h"
 
 // ADD THINGS HERE
+SC_HAS_PROCESS(kpn);
+kpn::kpn( ::sc_core::sc_module_name ): a(10), b(10), c(10), d(10), e("e")
+{
+    b.write(1);
+    c.write(0);
+    SC_THREAD(Delay);
+    SC_THREAD(Split);
+    SC_THREAD(Add);
+}
+
+void kpn::Add(){
+    while(true){
+        b.write(a.read()+c.read());
+    }
+}
+
+void kpn::Split() {
+    int i = 0;
+    while (i < 10) {
+        // wait(10, SC_NS);
+        unsigned int bt;
+        bt = b.read();
+        a.write(bt);
+        d.write(bt);
+        e.write(bt);
+        cout << "@" << sc_delta_count() << "δ: e = " << bt << endl;
+        i++;
+    }
+    sc_stop();
+}
+
+void kpn::Delay() {
+    while(true){
+        c.write(d.read());
+    }
+}
--- kpn.h
+++ kpn.h
@@ -2,17 +2,22 @@
 #define KPN_H
 
 #include <systemc.h>
+#include <iostream>
 
 SC_MODULE(kpn)
 {
 private:
-    // ADD THINGS HERE
+    sc_fifo<unsigned int> a;
+    sc_fifo<unsigned int> b;
+    sc_fifo<unsigned int> c;
+    sc_fifo<unsigned int> d;
+    sc_signal<unsigned int> e;
 
 public:
-    SC_CTOR(kpn) // : ADD THINGS HERE
-    {
-        // ADD THINGS HERE
-    }
+    kpn( ::sc_core::sc_module_name );
+    void Add();
+    void Split();
+    void Delay();
 };
 
 #endif // KPN_H
